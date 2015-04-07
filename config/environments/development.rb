a2be0b0103c45123d6c036f82f9d5811ac81602f
zchee <zcheeee@gmail.com>
2015-04-07 22:54:25 +0900

    Add Bullet and mini-profiler config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,26 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # http://ruby-rails.hatenadiary.com/entry/20150204/1423055537
+  config.after_initialize do
+    Bullet.enable  = true
+
+    Bullet.alert   = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    # Bullet.growl   = true      # Growl
+    # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #                 :password => 'bullets_password_for_jabber',
+    #                 :receiver => 'your_account@jabber.org',
+    #                 :show_online_status => true }
+    # Bullet.rails_logger = true
+    # Bullet.bugsnag      = true
+    # Bullet.airbrake     = true
+    Bullet.raise        = true
+    Bullet.add_footer   = true
+  end
+
+  Rack::MiniProfiler.config.position = 'right'
+
 end
