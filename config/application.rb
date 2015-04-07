750edfd475315d616730f8bbd885bc2db5212552
zchee <zcheeee@gmail.com>
2015-04-07 23:33:01 +0900

    Add specific file

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,5 @@ module Cheshire
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.web_console.whitelisted_ips          = '192.168.55.1'
   end
 end
---
+++ config/initializers/web_console.rb
@@ -0,0 +1,3 @@
+Rails.application.configure do
+  config.web_console.whitelisted_ips = '192.168.55.1'
+end
