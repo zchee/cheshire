f3dc905126f8c336da46ac962c136359ea312b00
zchee <zcheeee@gmail.com>
2015-04-08 12:44:51 +0900

    Fix user model validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   validates :name, presence: true
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :github_login, presence: true, uniqueness: { case_sensitive: false }
   validates :access_token, presence: true
   # has_secure_password
@@ -10,11 +11,11 @@ class User < ActiveRecord::Base
 
   # http://ruby-rails.hatenadiary.com/entry/20150221/1424489524
   # https://github.com/norman/friendly_id#what-changed-in-version-50
-  include FriendlyId
-  friendly_id :github_login, use: [:slugged, :finders]
-
-
-  def should_generate_new_friendly_id?
-    name_changed?
-  end
+  # include FriendlyId
+  # friendly_id :github_login, use: [:slugged, :finders]
+  #
+  #
+  # def should_generate_new_friendly_id?
+  #   name_changed?
+  # end
 end
