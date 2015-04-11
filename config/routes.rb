743fb70819f5b1bdf3605991f3a64b6b132f5ec3
zchee <zcheeee@gmail.com>
2015-04-11 12:17:03 +0900

    Refactoring static, Add authentiction

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, :only => [:new]
+  resources :sessions, :only => [:new, :create, :destroy]
   root to: 'home#index'
 
-  get '/signup', to: 'users#new', via: 'get'
-  get '/signin', to: 'sessions#new', via: 'get'
-  get '/signout', to: 'sessions#destroy', via: 'delete'
-  get '/about', to: 'home#about', via: 'get'
-  get '/contact', to: 'home#contact', via: 'get'
-  get '/feature', to: 'home#feature', via: 'get'
-  get '/help', to: 'home#help', via: 'get'
-  get '/privacy', to: 'home#privacy', via: 'get'
+  # static pages
+  get '/blog', to: 'home#blog'
+  get '/contact', to: 'home#contact'
+  get '/feature', to: 'home#feature'
+  get '/help', to: 'home#help'
+  get '/privacy', to: 'home#privacy'
 
+  # authentication
+  get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  delete '/signout', to: 'sessions#destroy'
+
+  # github auth
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
 
   mount Peek::Railtie => '/peek'
 end
