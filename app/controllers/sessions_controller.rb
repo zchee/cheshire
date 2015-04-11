c4e4b6eebe8c8cafae6bba388b001257a46512ff
zchee <zcheeee@gmail.com>
2015-04-11 12:16:05 +0900

    Add redirect_to on new route, and Add failure route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,21 @@
 class SessionsController < ApplicationController
 
   def new
+    redirect_to '/auth/github'
+  end
+
+
+  def create
   end
 
 
   def destroy
     sign_out
     redirect_to root_url
+  end
+
+
+  def failure
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
   end
 end
